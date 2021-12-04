c9a799855312502190a231c8659a2b44a8e004b7
ShimChang <95128916+ShimChang@users.noreply.github.com>
2021-12-04 22:33:28 +0900

    day3

--- 2751.cpp
+++ 2751.cpp
@@ -1,28 +1,17 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
-int main()
-{
+int main(){
     int n;
-    cin >> n;
-    vector<int> list(n);
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> list[i];
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
     }
-    for (int i = 1; i < n; i++)
-    {
-        int j=i-1;
-        int key = list[i];
-        while(j>=0&&list[j]<key){
-            list[j+1]=list[j];
-            j--;
-        }
-        list[j + 1]=key;
+    sort(v.begin(),v.end());
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<'\n';
     }
-    
-    for (int i = 0; i < n; i++)
-        cout << list[i] << '\n';
 }
