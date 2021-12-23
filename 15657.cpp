97b8f119f25cede9124efb02a1759a14eed1a5b0
ShimChang <95128916+ShimChang@users.noreply.github.com>
2021-12-23 22:48:56 +0900

    day13

---
+++ 15657.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int n,m;
+int arr[10];
+int arr2[10];
+
+void func(int k){ // 현재 k개까지 수를 택했음.
+  if(k == m){ // m개를 모두 택했으면
+    for(int i = 0; i < m; i++)
+      cout << arr[i] << ' '; // arr에 기록해둔 수를 출력
+    cout << '\n';
+    return;
+  }
+
+  for(int i = 0; i < n; i++){ // 1부터 n까지의 수에 대해
+    if(arr[k-1]<=arr2[i]){
+      arr[k] = arr2[i]; // k번째 수를 i로 정함
+
+      func(k+1); // 다음 수를 정하러 한 단계 더 들어감
+     
+    }
+    else continue;
+    
+  }
+}
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> m;
+  for(int i=0;i<n;i++){
+      cin>>arr2[i];
+  }
+  sort(arr2,arr2+n);
+  func(0);
+}
